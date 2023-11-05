e03667e5408dc0e4b4a82d04d850dbf2c26022d0
gopal518 <gopal180598@gmail.com>
2023-11-06 04:04:06 +0530

    Linkedlist

---
+++ dsa_Linkedlsit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+struct linkedList
+{
+    int data;
+    struct linkedlist *next;
+};
+
+int main(){
+    linkedList node;
+    head = (struct node *)malloc(sizeof(struct Node));
+    
+}
