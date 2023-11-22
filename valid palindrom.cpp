e49950a61573060d9a7f4be2ebdbb89a88dae0eb
gopal518 <gopal180598@gmail.com>
2023-11-22 16:20:29 +0530

    valid palindrom.cpp added

---
+++ valid palindrom.cpp
@@ -0,0 +1,48 @@
+// A phrase is a palindrome if,
+//     after converting all uppercase letters into lowercase letters and
+//     removing
+//             all non -
+//         alphanumeric characters,
+//     it reads the same forward and backward
+//         .Alphanumeric characters include letters and numbers.
+
+//     Given a string s,
+//     return true if it is a palindrome, or false otherwise.
+
+//                                            Example 1 :
+
+//     Input : s = "A man, a plan, a canal: Panama" Output : true Explanation
+//     : "amanaplanacanalpanama" is a palindrome.Example 2 :
+
+//     Input : s = "race a car" Output : false Explanation
+//     : "raceacar" is not a palindrome.Example 3 :
+
+//     Input : s = " " Output : true Explanation
+//     : s is an empty string "" after removing non -
+//       alphanumeric characters.Since an empty string reads the same forward
+//       and
+//                                        backward,
+//             it is a palindrome.
+
+//                 Constraints :
+
+//                 1 <= s.length <= 2 * 105
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start = 0
+        end = len(s) - 1
+        while start < end:
+            while start < end and not s[start].isalnum():
+                start += 1
+            while start < end and not s[end].isalnum():
+                end -= 1
+            if s[start].lower() != s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
