79b65ae137c20cb55d6d47bbee6e40235d3fb970
gopal518 <gopal180598@gmail.com>
2023-12-17 22:54:04 +0530

    added remove the element occur more than twice.py

---
+++ remove the element occur more to twice.py
@@ -0,0 +1,67 @@
+'''Given an integer array nums sorted in non-decreasing order, remove some duplicates in-place such that each unique element appears at most twice. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3]
+Output: 5, nums = [1,1,2,2,3,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,1,2,3,3]
+Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+Explanation: Your function should return k = 7, with the first seven elements of nums being 0, 0, 1, 1, 2, 3 and 3 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).'''
+
+
+# in place array space complexity 0(1) time complexity 0(n^2)
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        p1 = 0
+        p2 = 1
+        shift = 0
+        while(p1<len(nums)):
+            val = nums[p1]
+            count = 0
+
+            while( p2<len(nums) and val == nums[p2]):
+                count+=1
+                p2+=1
+            if count<=1:
+                p1 = p2
+                p2+=1
+            else:
+                while(p2<len(nums)):
+                    nums[p2-count+1],nums[p2]= nums[p2],nums[p2-count+1]
+                    p2+=1
+                p2 = p1 +1
+                shift = shift + count - 1
+        return (len(nums)-shift)
+        
