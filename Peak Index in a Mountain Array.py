8f986d12d8806b3d6acaa1315e1e3f91ddf02658
gopal518 <gopal180598@gmail.com>
2024-01-25 23:07:23 +0530

    return when gradiant direction gets negative

--- Peak Index in a Mountain Array.py
+++ Peak Index in a Mountain Array.py
@@ -43,3 +43,12 @@ class Solution:
                 max_ = j
         return index
         
+### Time complexity O(N) and Space complexity O(1) but at early exit 
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        index = -1
+        max_ = -1
+        for i in range(len(arr)):
+            if (arr[i]>arr[i+1]):
+                return i
+        
